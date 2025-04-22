06ac6b9c1393a68a3e6356be31bfc2bd14be7d08
SofmxTran <sonth.24ce@vku.udn.vn>
2025-04-23 01:01:54 +0700

    Initial commit

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -5,12 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // Sửa tên biến từ ULR thành URL
+
     private static final String URL = "jdbc:mysql://localhost:3306/dormitory?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-    // Đăng ký driver JDBC (đảm bảo driver có sẵn trong classpath)
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
